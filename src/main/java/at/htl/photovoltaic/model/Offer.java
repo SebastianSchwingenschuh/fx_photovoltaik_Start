bf37730bab55a02e6db131ad5a71907a37e9ce15
SebastianSchwingenschuh <sebastianschwingii@gmail.com>
2026-05-20 15:35:57 +0200

    offer

--- src/main/java/at/htl/photovoltaic/model/Offer.java
+++ src/main/java/at/htl/photovoltaic/model/Offer.java
@@ -1,4 +1,61 @@
 package at.htl.photovoltaic.model;
 
 public class Offer {
+    private int id;
+    private String name;
+    private int numberOfPanels;
+    private double powerPerPanel;
+    private double totalPrice;
+
+    public Offer(String name, int numberOfPanels, double powerPerPanel, double totalPrice) {
+        this.name = name;
+        this.numberOfPanels = numberOfPanels;
+        this.powerPerPanel = powerPerPanel;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfPanels() {
+        return numberOfPanels;
+    }
+
+    public void setNumberOfPanels(int numberOfPanels) {
+        this.numberOfPanels = numberOfPanels;
+    }
+
+    public double getPowerPerPanel() {
+        return powerPerPanel;
+    }
+
+    public void setPowerPerPanel(double powerPerPanel) {
+        this.powerPerPanel = powerPerPanel;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d: %s, %f kWp, %f.2 €", getId(), getName(), getNumberOfPanels() * getPowerPerPanel() / 1000, getTotalPrice());
+    }
 }
